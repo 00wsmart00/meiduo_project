1d80aeca52dbcad3e5fc92e2df2a14db3329ef61
00wsmart00 <2767323267@qq.com>
2019-06-12 13:46:04 +0800

    '用户模型类创建并迁移'

--- meiduo_mall/meiduo_mall/apps/users/models.py
+++ meiduo_mall/meiduo_mall/apps/users/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    """自定义用户模型"""
+
+    # 在用户模型的基础上增加mobile字段
+    mobile = models.CharField(max_length=11, unique=True)
+
+    # 对当前的表进行相关设置
+    class Meta:
+        db_table = 'tb_users'
+        verbose_name = '用户'
+        verbose_name_plural = verbose_name
+
+    # 在str魔法方法中，返回用户名称
+    def __str__(self):
+        return self.username
