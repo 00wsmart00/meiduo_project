b1ea0d46210c23f7afa077579dec2074462e7d78
00wsmart00 <2767323267@qq.com>
2019-06-12 22:28:29 +0800

    '完成注册功能,异步方案未完成'

--- meiduo_mall/meiduo_mall/apps/users/urls.py
+++ meiduo_mall/meiduo_mall/apps/users/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     # 注册
-    url(r'^register/$', views.RegisterView.as_view())
+    url(r'^register/$', views.RegisterView.as_view()),
+    url(r'^usernames/(?P<username>\w{5,20})/count/$', views.UsernameCountView.as_view()),
+    # 判断手机号是否重复
+    url(r'^mobiles/(?P<mobile>1[3-9]\d{9})/count/$', views.MobileCountView.as_view()),
 ]
--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,7 +1,58 @@
-from django.shortcuts import render
+import re
+
+from django import http
+from django.contrib.auth import login
+from django.db import DatabaseError
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.urls import reverse
 from django.views import View
+from django_redis import get_redis_connection
+
+from users.models import User
+from meiduo_mall.utils.response_code import RETCODE
+
+
+class MobileCountView(View):
+    """判断手机号是否被注册"""
+
+    def get(self, request, mobile):
+        """
+
+        :param request:
+        :return: Json
+        """
+        # 数据库去查询
+        count = User.objects.filter(username=mobile).count()
+
+        # 返回
+        return http.JsonResponse({
+            'code': RETCODE.OK,
+            'errmsg': 'ok',
+            'count': count
+        })
+
+
+class UsernameCountView(View):
+    """判断用户名是否重复注册"""
+
+    def get(self, request, username):
+        """
+
+        :param request:
+        :return: Json
+        """
+        # 获取参数
+        # 数据库去查询
+        count = User.objects.filter(username=username).count()
+
+        # 返回
+        return http.JsonResponse({
+            'code': RETCODE.OK,
+            'errmsg': 'ok',
+            'count': count
+        })
 
 
 class RegisterView(View):
@@ -15,3 +66,61 @@ class RegisterView(View):
         """
         return render(request, 'register.html')
 
+    def post(self, request):
+        """提交注册页面"""
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        mobile = request.POST.get('mobile')
+        allow = request.POST.get('allow')
+        sms_code_client = request.POST.get('sms_code')
+
+        # 判断参数是否齐全
+        if not all([username, password, password2, mobile, allow, sms_code_client]):
+            return http.HttpResponseForbidden('缺少必传参数')
+        # 判断用户名是否是5-20个字符
+        if not re.match(r'^[a-zA-Z0-9_-]{5,20}$', username):
+            return http.HttpResponseForbidden('请输入5-20个字符的用户名')
+        # 判断密码是否是8-20个数字
+        if not re.match(r'^[0-9A-Za-z]{8,20}$', password):
+            return http.HttpResponseForbidden('请输入8-20位的密码')
+        # 判断两次密码是否一致
+        if password != password2:
+            return http.HttpResponseForbidden('两次输入的密码不一致')
+        # 判断手机号是否合法
+        if not re.match(r'^1[3-9]\d{9}$', mobile):
+            return http.HttpResponseForbidden('请输入正确的手机号码')
+        # 判断是否勾选用户协议
+        if allow != 'on':
+            return http.HttpResponseForbidden('请勾选用户协议')
+
+        # 获取redis链接对象
+        redis_connection = get_redis_connection('verify_code')
+
+        # 从redis中获取保存的sms_code
+        sms_code_server = redis_connection.get('sms_code_%s' % mobile)
+
+        # 判断sms_code_server是否存在
+        if sms_code_server is None:
+            # 不存在直接返回, 说明服务器的过期了, 超时
+            return render(request,
+                          'register.html',
+                          {'sms_code_errmsg': '无效的短信验证码'})
+        # 如果存在,对比两者
+        if sms_code_client != sms_code_server.decode():
+            # 对比失败, 说明短信验证码有问题, 直接返回:
+            return render(request,
+                          'register.html',
+                          {'sms_code_errmsg': '输入短信验证码有误'})
+
+        # 保存注册数据
+        try:
+            user = User.objects.create_user(username=username, password=password, mobile=mobile)
+        except DatabaseError:
+            return render(request, 'register.html', {'register_errmsg': '注册失败'})
+
+        # 实现状态保持
+        login(request, user)
+
+        # 响应注册结果
+        return redirect(reverse('contents:index'))
